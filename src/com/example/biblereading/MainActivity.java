aa38152dc0b7a6d56ddd24e790f73247aca20776
Zhehan Xue <z9xue@uwaterloo.ca>
2015-09-03 17:07:30 -0400

    pick strat time and end time

--- src/com/example/biblereading/MainActivity.java
+++ src/com/example/biblereading/MainActivity.java
@@ -6,31 +6,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+import android.graphics.Typeface;
+
+import android.widget.TextView;
+
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Typeface face0 = Typeface.createFromAsset(getAssets(),"fonts/fonts1.TTF");
+        TextView PickDateTitle = (TextView) findViewById(R.id.PickDateTitle);
+        PickDateTitle.setTypeface(face0);
+        
+        Typeface face1 = Typeface.createFromAsset(getAssets(),"fonts/fonts2.TTF");
+        TextView StartDate = (TextView) findViewById(R.id.StartDate);
+        StartDate.setTypeface(face1);
+        TextView EndDate = (TextView) findViewById(R.id.EndDate);
+        EndDate.setTypeface(face1);
+        
+        
+        
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   
 }
